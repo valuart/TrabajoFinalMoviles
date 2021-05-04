565ce0db20fcc9522852e8208fe0af5e444cb05a
Lucila Lillo <lucilaslillo@gmail.com>
2021-05-04 17:53:08 -0300

    comentarios

--- app/src/main/java/com/example/plantilla/ui/ui/Inquilino/InquilinosFragment.java
+++ app/src/main/java/com/example/plantilla/ui/ui/Inquilino/InquilinosFragment.java
@@ -42,36 +42,6 @@ public class InquilinosFragment extends Fragment {
     Button ver;
     private Context context;
 
-   /* // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public InquilinosFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment InquilinosFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    /*public static InquilinosFragment newInstance(String param1, String param2) {
-        InquilinosFragment fragment = new InquilinosFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }*/
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
