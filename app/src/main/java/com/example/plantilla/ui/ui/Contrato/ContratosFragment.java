a1bf9545c404f5440d6fef2886d1ac51fd7e34d4
valuart <42857715+valuart@users.noreply.github.com>
2021-05-04 17:16:04 -0300

    contrato

--- app/src/main/java/com/example/plantilla/ui/ui/Contrato/ContratosFragment.java
+++ app/src/main/java/com/example/plantilla/ui/ui/Contrato/ContratosFragment.java
@@ -5,60 +5,99 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
-import androidx.navigation.Navigation;
 
 
 import com.example.plantilla.R;
 import com.example.plantilla.modelo.Contrato;
 import com.example.plantilla.modelo.Inmueble;
-import com.example.plantilla.ui.ui.Inmueble.InmueblesFragment;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class ContratosFragment extends Fragment {
-    private ListView lvc;
-    ArrayList<Inmueble> lista = new ArrayList<>();
-    private ContratosFragmentViewModel Cvm;
+    private ContratosFragmentViewModel cvm;
+    private Context contexto;
+    private ContratoAdapter contratoAdapter;
+    private  ListView LvContratos;
+    private ContratoFragment c;
+
+
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public ContratosFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment InmueblesFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static ContratosFragment newInstance(String param1, String param2) {
+        ContratosFragment fragment = new ContratosFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Cvm = ViewModelProvider.AndroidViewModelFactory.getInstance(getActivity().getApplication()).create(ContratosFragmentViewModel.class);
-       /* Cvm.getListaDirecciones().observe(this, (Observer<? super ArrayList<Contrato>>) new Observer<List<String>>() {
-            @Override
-            public void onChanged(List<String> strings) {
-                ArrayAdapter<String> adapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_list_item_1, strings);
-                lvc.setAdapter(adapter);
-            }
-        })*/;
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
     }
-
     @Override
+
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_item_contrato, container, false);
-        lvc= view.findViewById(R.id.listaInmuContratos);
-        Cvm.cargarDatos();
-        lvc.setClickable(true);
-        lvc.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        View root=  inflater.inflate(R.layout.fragment_contratos, container, false);
+        contexto = root.getContext();
+        inicializarVista(root);
+
+        return root;
+    }
+
+    private void inicializarVista(View v) {
+        LvContratos = v.findViewById(R.id.listaInmuContratos);
+
+        cvm = ViewModelProvider.AndroidViewModelFactory.getInstance(getActivity().getApplication()).create(ContratosFragmentViewModel.class);
+
+        cvm.cargarDatos();
+
+        cvm.getListaDirecciones().observe(getViewLifecycleOwner(), new Observer<List<Contrato>>() {
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                TextView tv = (TextView)view;
-                Bundle bundle = new Bundle();
-                bundle.putString("direccion", tv.getText().toString());
-               // Navigation.findNavController(view).navigate(R.id.contrato, bundle);
+            public void onChanged(List<Contrato> c) {
+                //GridLayoutManager gridLayoutManager = new GridLayoutManager(contexto,3,GridLayoutManager.VERTICAL,false);
+
+                contratoAdapter = new ContratoAdapter(contexto,1,c,getLayoutInflater());
+                LvContratos.setAdapter(contratoAdapter);
+
             }
         });
-        return view;
+
+
+
     }
+
 }
