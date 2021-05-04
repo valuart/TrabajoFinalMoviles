222e343628a4a1f8631f755c8f0c75adb46ff1f9
Lucila Lillo <lucilaslillo@gmail.com>
2021-05-04 17:39:18 -0300

    inicializarvista

--- app/src/main/java/com/example/plantilla/ui/ui/Inquilino/InquilinosFragment.java
+++ app/src/main/java/com/example/plantilla/ui/ui/Inquilino/InquilinosFragment.java
@@ -1,6 +1,7 @@
 package com.example.plantilla.ui.ui.Inquilino;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -17,6 +18,7 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
 
+import com.bumptech.glide.Glide;
 import com.example.plantilla.R;
 import com.example.plantilla.modelo.Inmueble;
 import com.example.plantilla.modelo.Inquilino;
@@ -26,7 +28,7 @@ import com.example.plantilla.ui.ui.Inmueble.InmueblesFragmentViewModel;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
+/*
  * A simple {@link Fragment} subclass.
  * Use the {@link InquilinosFragment#newInstance} factory method to
  * create an instance of this fragment.
@@ -40,7 +42,7 @@ public class InquilinosFragment extends Fragment {
     Button ver;
     private Context context;
 
-    // TODO: Rename parameter arguments, choose names that match
+   /* // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
@@ -62,15 +64,14 @@ public class InquilinosFragment extends Fragment {
      * @return A new instance of fragment InquilinosFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static InquilinosFragment newInstance(String param1, String param2) {
+    /*public static InquilinosFragment newInstance(String param1, String param2) {
         InquilinosFragment fragment = new InquilinosFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
-    }
-
+    }*/
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -90,6 +91,9 @@ public class InquilinosFragment extends Fragment {
                 List<Inmueble> lista = (List<Inmueble>) ifvm.getListaInmuebleMutable();
                 ArrayAdapter<Inmueble> adapter = new ArrayAdapter<Inmueble>(getContext(), android.R.layout.simple_list_item_1, lista);
                 listaInmuebles.setAdapter(adapter);
+                /*Glide.with(getContext())
+                        .load()*/
+                //aca debo cargar la direccion y la foto usando glide
             }
         });
         return root;
@@ -98,6 +102,16 @@ public class InquilinosFragment extends Fragment {
     private void inicializarVista(View v) {
         listaInmuebles = v.findViewById(R.id.listaInmuebles);
         //deberia mostrar la direccion, la foto del inmueble y con el boton ver, ir a los datos del inquilino
+        direccion = v.findViewById(R.id.etDireccion);
+        foto = v.findViewById(R.id.imagen);
+        ver = v.findViewById(R.id.btnVer);
+        ver.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getContext(), InquilinoFragment.class);
+                startActivity(i);
+            }
+        });
     }
 }
 
